0448f921ad0758137e543fdef3a4f5b2df831dcf
1706903 <1706903@LABBWS-01.STUDENTS.utech.edu.jm>
2019-07-06 14:47:16 -0500

    added today object

--- OOP+PROJECT/src/car4rent/Car4Rent_Main.java
+++ OOP+PROJECT/src/car4rent/Car4Rent_Main.java
@@ -5,6 +5,7 @@ public class Car4Rent_Main {
 	public static void main(String[] args) {
 		
 		Vehicles ford1 = new Vehicles("r","a", "t", 1, "a", "t", 'A', 4.23, 1, 4.33, "u");
+		Date today = new Date(11, 11, 2019);
 		
 
 	}
