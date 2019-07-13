37e2412fbce60e917c363b3e0ec4901e3aa49a1c
1706903 <1706903@LABCWS-02.STUDENTS.utech.edu.jm>
2019-07-13 13:22:35 -0500

    added a few gui features

--- OOP+PROJECT/src/car4rent/Car4Rent_Main.java
+++ OOP+PROJECT/src/car4rent/Car4Rent_Main.java
@@ -1,12 +1,47 @@
 package car4rent;
 
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
 public class Car4Rent_Main {
 
 	public static void main(String[] args) {
 		
-		Vehicles ford1 = new Vehicles("r","a", "t", 1, "a", "t", 'A', 4.23, 1, 4.33, "u");
-		Date today = new Date(11, 11, 2019);
-		User dkfd = new User();
+		//Creating the Frame
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        
+        //Creating the MenuBar and adding components
+        JMenuBar menuBar = new JMenuBar();
+        JMenu Home = new JMenu("HOME");
+        JMenu ViewVehicle = new JMenu("View Vehicles");
+        menuBar.add(Home);
+        menuBar.add(ViewVehicle);
+        JMenuItem open = new JMenuItem("Open");
+        JMenuItem save = new JMenuItem("Save");
+        Home.add(open);
+        Home.add(save);
+        
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter ");
+        JButton enter = new JButton("ENTER");
+        JButton reset = new JButton("RESET");
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(enter);
+        panel.add(reset);
+        
+        // Text Area at the Center
+        JTextArea textArea = new JTextArea();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, menuBar);
+        frame.getContentPane().add(BorderLayout.CENTER, textArea);
+        frame.setVisible(true);
 
 	}
 
