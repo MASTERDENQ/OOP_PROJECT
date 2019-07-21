a53bea2a9f54d1e2ab19c4e58fcfa27683d39062
1706903 <1706903@LABCWS-01.STUDENTS.utech.edu.jm>
2019-07-21 15:39:26 -0500

    defer

---
+++ OOP+PROJECT/src/car4rent/Dummy.java
@@ -0,0 +1,48 @@
+package car4rent;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+
+public class Dummy extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Dummy frame = new Dummy();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Dummy() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		table = new JTable();
+		table.setToolTipText("");
+		table.setBounds(47, 225, 324, -112);
+		contentPane.add(table);
+	}
+}
